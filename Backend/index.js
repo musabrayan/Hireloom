873055b14308af324894827c71c01963d1047c64
musabrayan <musabrayan1703@gmail.com>
2025-05-23 21:42:46 +0530

    Set up Express server with MongoDB connection and user model

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from "./utils/db.js"
+
+dotenv.config({})
 const app = express();
 
 //middleware
@@ -12,9 +16,11 @@ const corsOptions = {
     credentials:true
 }
 app.use(cors(corsOptions));
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
     
 })
