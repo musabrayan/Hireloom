3d6c7bf9ffb6a6a735f906e24d645271e18acc46
musabrayan <musabrayan1703@gmail.com>
2025-06-16 00:25:12 +0530

    minor fix

--- Frontend/src/components/UserProfile.jsx
+++ Frontend/src/components/UserProfile.jsx
@@ -7,7 +7,7 @@ import { Contact, Mail, Pen } from 'lucide-react';
 import { Badge } from './ui/badge';
 import { Label } from './ui/label';
 import MyJobApplications from './MyJobApplications';
-import UpdateProfile from './UpdateProfile';
+import UpdateProfile from '@/components/UpdateProfile';
 import { useSelector } from 'react-redux';
 import useUserAppliedJobs from '@/hooks/useUserAppliedJobs';
 
